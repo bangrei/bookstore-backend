90cfdcd9988dd0d96e88bfe8db722c33a85a7968
naldi <syafrinaldi@eunoia.asia>
2024-04-02 16:41:16 +0700

    bugfix for vercel deployment

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,19 @@ import { AppDataSource } from "./data-source"
 import * as express from "express";
 import * as dotenv from "dotenv";
 import apiRouter from "./routes/api";
-import * as cors from 'cors';
+import * as cors from "cors";
 
-import { Request, Response } from "express";
+//import { Request, Response } from "express";
 import "reflect-metadata";
-import seed from "./seed/bookSeed";
-import userSeed from './seed/userSeed';
+//import seed from "./seed/bookSeed";
+//import userSeed from './seed/userSeed';
 
-dotenv.config();
-
-const app = express();
+const app = express.default();
 app.use(express.json());
-app.use(cors());
+app.use(cors.default());
 
 app.use("/api", apiRouter);
+dotenv.config();
 
 const { PORT = 5000 } = process.env;
 
@@ -23,12 +22,15 @@ app.get("/", (req, res) => {
   res.send(`PAGE NOT FOUND!!`);
 });
 
-AppDataSource.initialize().then(async () => {
+AppDataSource.initialize()
+  .then(async () => {
     // await seed.bookseed();
     // await userSeed.seed();
     app.listen(PORT, () => {
       console.log("Server is running on http://localhost:" + PORT);
     });
     console.log("Data Source has been initialized!");
+  })
+  .catch((error) => console.log(error));
 
-}).catch(error => console.log(error))
+export default app;
